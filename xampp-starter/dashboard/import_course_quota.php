582b200308519522c2853e97b55dee5ce5b09edd
AliSandro104 <alisandro2000@hotmail.com>
2022-11-29 21:34:08 -0500

    implemented the import functionality

--- xampp-starter/dashboard/import_course_quota.php
+++ xampp-starter/dashboard/import_course_quota.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // Change accordingly
 $username = "root"; // Change accordingly
 $password = ""; // Change accordingly
-$db = "courses_quota"; // Change accordingly
+$db = "ta-management"; // Change accordingly
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
@@ -11,21 +11,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
 // Missing a lot of error checks
-if(isset($_FILES['file'])){
-    $file_content = file($_FILES['file']['tmp_name']);
+if(isset($_FILES['myfile'])){
+    $file_content = file($_FILES['myfile']['tmp_name']);
     foreach($file_content as $row) {
         $items = explode(",", trim($row));
-        $course_number = $items[4];
-        $course_name = $items[0];
-        $course_description = $items[1];
-        $course_term = $items[2];
-        $course_year = $items[3];
-        $course_instructor_email = $items[5];
-
-        $sql = $conn->prepare("INSERT INTO Course (courseName, courseDesc, term, year, courseNumber, courseInstructor) VALUES (?, ?, ?, ?, ?, ?)");
-        $sql->bind_param('ssssss', $course_name, $course_description, $course_term, $course_year, $course_number, $course_instructor_email);
+        $course_term = $items[0];
+        $course_number = $items[1];
+        $course_name = $items[2];
+        $course_type = $items[3];
+        $instructor_name = $items[4];
+        $enrollment_number = $items[5];
+        $ta_quota = $items[6];
+        
+        $sql = $conn->prepare("INSERT INTO courses_quota (TermYear, CourseNumber, CourseName, CourseType, InstructorName, EnrollmentNumber, TAQuota) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $sql->bind_param('sssssii', $course_term, $course_number, $course_name, $course_type, $instructor_name, $enrollment_number, $ta_quota);
         $result = $sql->execute();
     }
 }
---
+++ xampp-starter/dashboard/import_ta_cohort.php
@@ -0,0 +1,45 @@
+<?php
+$servername = "localhost"; // Change accordingly
+$username = "root"; // Change accordingly
+$password = ""; // Change accordingly
+$db = "ta-management"; // Change accordingly
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Missing a lot of error checks
+if(isset($_FILES['myfile'])){
+    $file_content = file($_FILES['myfile']['tmp_name']);
+    foreach($file_content as $row) {
+        $items = explode(",", trim($row));
+        $course_term = $items[0];
+        $ta_name = $items[1];
+        $student_id = $items[2];
+        $legal_name = $items[3];
+        $email = $items[4];
+        $grad_ugrad = $items[5];
+        $supervisor_name = $items[6];
+        $priority = $items[7];
+        $hours = $items[8];
+        $date_applied = $items[9];
+        $location = $items[10];
+        $phone = $items[11];
+        $degree = $items[12];
+        $courses_applied = $items[13];
+        $open_to_other_courses = $items[14];
+        $notes = $items[15];
+        
+        $sql = $conn->prepare("INSERT INTO ta_cohort (TermYear, TAName, StudentID, 	LegalName, Email, GradUgrad, SupervisorName, Priority, NumberHours, DateApplied, TheLocation, Phone, Degree, CoursesApplied, OpenToOtherCourses, Notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $sql->bind_param('ssisssssississss', $course_term, $ta_name, $student_id, $legal_name, $email, $grad_ugrad, $supervisor_name, $priority, $hours, $date_applied, $location, $phone, $degree, $courses_applied, $open_to_other_courses, $notes);
+        $result = $sql->execute();
+    }
+}
+$conn->close();
+header("Location: admin.html");
+exit();
+
+?>
