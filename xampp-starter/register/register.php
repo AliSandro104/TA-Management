b54d3e7a6f9570bbcd70e57accf8537d852ac4aa
unknown <moustapha.moumneh@mail.mcgill.ca>
2022-11-29 23:16:16 -0500

    redirect

--- xampp-starter/register/register.php
+++ xampp-starter/register/register.php
@@ -6,27 +6,27 @@
     $username = "root";
     $db_password = "";
     $dbname = "users";
-    echo "here"
+
     // Connect to database and check if successful
-    $con = mysqli_connect($servername, $username, $password, $dbname) or die("unable to connect to host");
+    $con = mysqli_connect($servername, $username, $db_password, $dbname);
 
 
 if(isset($_POST['submit'])){
 
-    $fname = $_POST['fname']
-    $lname = $_POST['lname']
-    $email = $_POST['email']
-    $password = $_POST['password']
-    $studentID = check_input($_POST['sID'], "Optional");
-    $student = $_POST['isStudent']
-    $professor = $_POST['isProf']
-    $ta = $_POST['isTA']
-    $admin = $_POST['isAdmin']
-    $courses = $_POST['courses'];
-    $time = date_default_timezone_get();
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $studentID = $_POST['sID'] ?? false;
+    $student = $_POST['isStudent'] ?? false;
+    $professor = $_POST['isProf'] ?? false;
+    $ta = $_POST['isTA'] ?? false;
+    $admin = $_POST['isAdmin'] ?? false;
+    $courses = $_POST['courses'] ?? false;
+    $time = date('d-m-y h:i:s');
 
     if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)
-         && isset($student || $professor || $ta || $admin) !empty($courses))
+         && (null !== ($student || $professor || $ta || $admin)) && !empty($courses))
 		{
 			//save to general user database
 			$query1 = "insert into user (firstName,lastName,email,password,createdAt,updatedAt) 
@@ -35,35 +35,35 @@ if(isset($_POST['submit'])){
 			mysqli_query($con, $query1);
 
             #add to professor table if user is a professor and update index in user_usertype
-            if (!empty ($professor))
-                $query2= "insert into professor (professor) values ('$email')"
-                mysqli_query($con, $query2);
+            if (!empty ($professor)){
+                $query2= "insert into professor (professor) values ('$email')";
+                //mysqli_query($con, $query2);
 
-                $query2= "insert into user_usertype (userId, userTypeId) values ('$email',2)"
-                mysqli_query($con, $query2);
+                $query2= "insert into user_usertype (userId, userTypeId) values ('$email',2)";
+                mysqli_query($con, $query2);}
             
             #add to student table if user is a student and update index in user_usertype
-            if (!empty ($student))
-                $query3= "insert into students (email,studentID) values ('$email','$studentID')"
+            if (!empty ($student)){
+                $query3= "insert into students (email,studentID) values ('$email','$studentID')";
                 mysqli_query($con, $query3);
 
-                $query3= "insert into user_usertype (userId, userTypeId) values ('$email',1)"
-                mysqli_query($con, $query3);
+                $query3= "insert into user_usertype (userId, userTypeId) values ('$email',1)";
+                mysqli_query($con, $query3);}
             
             #if user is a ta update index in user_usertype
-            if (!empty ($ta))
+            if (!empty ($ta)){
 
-                $query4= "insert into user_usertype (userId, userTypeId) values ('$email',3)"
-                mysqli_query($con, $query4);
+                $query4= "insert into user_usertype (userId, userTypeId) values ('$email',3)";
+                mysqli_query($con, $query4);}
             
             #if user is admin update index in user_usertype
-            if (!empty ($admin))
+            if (!empty ($admin)){
 
-                $query5= "insert into user_usertype (userId, userTypeId) values ('$email',4)"
-                mysqli_query($con, $query5);
+                $query5= "insert into user_usertype (userId, userTypeId) values ('$email',4)";
+                mysqli_query($con, $query5);}
 
             #add list of courses to user_courses table
-            $query6= "insert into user_courses (email, courses) values ('$email',$courses)"
+            $query6= "insert into user_courses (email, courses) values ('$email',$courses)";
             mysqli_query($con, $query6);
 
             //redirect to login after successful register
